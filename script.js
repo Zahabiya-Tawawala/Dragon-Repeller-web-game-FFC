298e75d3bfb8008dcd87b40b0865dc4480af208a
Zahabiya Tawawala <zahabiyatawawala1@gmail.com>
2024-03-26 23:49:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,6 +28,13 @@ const locations = [
     "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)","Go to town square"],
     "button functions": [buyHealth, buyWeapon, goTown],
     text: "You enter the store."
+},
+{
+    
+    name: "cave",
+    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+    "button functions": [fightSlime, fightBeast, goTown],
+    text: "You enter the cave. You see some monsters."
 }
 
 ];
@@ -56,7 +63,7 @@ function goStore(){
 }
 
 function goCave() {
-    console.log("Going to cave.");
+    update(locations[2])
 }
 
 function fightDragon() {
@@ -68,5 +75,13 @@ function buyHealth(){
 }
 
 function buyWeapon(){
+
+}
+
+function fightSlime(){
+
+}
+
+function fightBeast(){
 
 }
